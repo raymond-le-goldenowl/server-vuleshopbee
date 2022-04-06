4d8de805a30cd48f7a34f16ad22c3c192c030469
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2022-04-06 12:05:14 +0700

    connect database (pg)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
