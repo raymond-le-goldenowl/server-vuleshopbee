ca0be4a56f13081adeef545a1744ad2d7b27274e
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2022-04-12 15:39:52 +0700

    feat: create, read, update, delete, get image of menu entity

--- src/menu/menu.module.ts
+++ src/menu/menu.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
-import { MenuService } from './menu.service';
-import { MenuController } from './menu.controller';
-import { Menu } from './entities/menu.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { MenuService } from './menu.service';
+import { MenuController } from './menu.controller';
+import { MenuRepository } from './menu.repository';
+import { CategoriesRepository } from 'src/categories/categories.repository';
+import { CategoriesService } from 'src/categories/categories.service';
+import { CategoriesModule } from 'src/categories/categories.module';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Menu])],
+  imports: [TypeOrmModule.forFeature([MenuRepository]), CategoriesModule],
   controllers: [MenuController],
+
   providers: [MenuService],
 })
 export class MenuModule {}
