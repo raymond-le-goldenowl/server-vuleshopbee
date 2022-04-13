ff92acec88bdc6dd844ba4eb2aa2a603afcd727e
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2022-04-13 11:55:55 +0700

    feat: create, read, update, delete, get image of product entity

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,23 +1,35 @@
+import { FileInterceptor } from '@nestjs/platform-express';
 import {
-  Controller,
   Get,
   Post,
   Body,
+  Query,
   Patch,
   Param,
   Delete,
+  Controller,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
+
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { config } from './file-interceptor.config';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @UseInterceptors(FileInterceptor(config.fieldName, config.localOptions))
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  create(@UploadedFile() image, @Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(image, createProductDto);
+  }
+
+  @Get('/filters')
+  filterProducts(@Query() query) {
+    return this.productsService.findAllWithSearch(query);
   }
 
   @Get()
@@ -27,16 +39,21 @@ export class ProductsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+    return this.productsService.findOne(id);
   }
 
+  @UseInterceptors(FileInterceptor(config.fieldName, config.localOptions))
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(
+    @UploadedFile() image,
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(image, id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+  remove(@Param('id') id: string, @Query('remove') remove: boolean) {
+    return this.productsService.remove(id, remove);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,215 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { ProductsRepository } from './products.repository';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { CategoriesService } from 'src/categories/categories.service';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(ProductsRepository)
+    private productsRepository: ProductsRepository,
+    private categoriesService: CategoriesService,
+  ) {}
+
+  async create(image: any, createProductDto: CreateProductDto) {
+    const {
+      name,
+      model,
+      original_price,
+      price,
+      tutorial,
+      description,
+      platform,
+      status,
+      sale_of,
+      variant_id,
+      variant_title,
+      variant_text,
+      additional_information,
+      amount,
+      category_id,
+    } = createProductDto;
+
+    if (!image?.filename) {
+      throw new BadRequestException();
+    }
+
+    const path = `/products/images/${image?.filename}`;
+    const slug = name.split(' ').join('-');
+    const category = await this.categoriesService.findOne(category_id);
+    const productSaved = await this.productsRepository.save({
+      name,
+      model,
+      original_price,
+      price,
+      slug: slug,
+      image: path,
+      tutorial,
+      description,
+      platform,
+      status,
+      sale_of,
+      variant_id,
+      variant_title,
+      variant_text,
+      additional_information,
+      amount,
+      category,
+    });
+
+    if (!productSaved) {
+      throw new BadRequestException();
+    }
+
+    return productSaved;
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    const products = await this.productsRepository.find({ withDeleted: true });
+    return products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findAllWithSearch(query) {
+    // create builder.
+    const builder = await this.productsRepository.createQueryBuilder('product');
+
+    // join with categories.
+    if (query?.category_id) {
+      const category_id = query?.category_id;
+      builder
+        .leftJoinAndSelect('product.category', 'category')
+        .andWhere('category.id = :category_id', {
+          category_id: category_id,
+        });
+    }
+
+    // join with tag.
+    if (query?.tag) {
+      const tag = query?.tag;
+      builder
+        .leftJoinAndSelect('product.tags', 'tag')
+        .andWhere('tag.text LIKE :tag', {
+          tag: `%${tag}%`,
+        });
+    }
+
+    // Filter range price
+    if (query?.price_from && query?.price_to) {
+      const price_from = query?.price_from;
+      const price_to = query?.price_to;
+      builder.andWhere('product.price BETWEEN :price_from AND :price_to', {
+        price_from,
+        price_to,
+      });
+    } else if (query?.price_from) {
+      const price_from = query?.price_from;
+      builder.andWhere('product.price >= :price_from', {
+        price_from,
+      });
+    } else if (query?.price_to) {
+      const price_to = query?.price_to;
+      builder.andWhere('product.price <= :price_to', {
+        price_to,
+      });
+    }
+
+    /** SORTING
+     * sort=sales-desc: bán chạy nhất
+     * sort=date-desc: Mới cập nhập.
+     * sort=date-asc: cập nhập cũ nhất.
+     * price-asc: giá thấp đến cao.
+     * price-desc: giá cao đến thấp.
+     * name-asc: từ A đến Z.
+     * name-desc: từ Z đến A.
+     */
+    if (query?.sort && query?.sort === 'date-desc') {
+      builder
+        .orderBy('product.created_at', 'DESC')
+        .addOrderBy('product.updated_at', 'DESC');
+    } else if (query?.sort && query?.sort === 'date-asc') {
+      builder
+        .orderBy('product.created_at', 'ASC')
+        .addOrderBy('product.updated_at', 'ASC');
+    } else if (query?.sort && query?.sort === 'price-asc') {
+      builder.orderBy('product.price', 'ASC');
+    } else if (query?.sort && query?.sort === 'price-desc') {
+      builder.orderBy('product.price', 'DESC');
+    } else if (query?.sort && query?.sort === 'name-asc') {
+      builder.orderBy('product.name', 'ASC');
+    } else if (query?.sort && query?.sort === 'name-desc') {
+      builder.orderBy('product.name', 'DESC');
+    }
+
+    // pagination
+    const page = parseInt(query?.page) || 1;
+    const perPage = parseInt(query?.per_page) || 8;
+    //* OFFSET may not work as you may expect if you are using complex queries with joins or subqueries.
+    //* If you are using pagination, it's recommended to use *skip* instead.
+    // builder.offset((page - 1) * perPage).limit(perPage);
+    builder.skip((page - 1) * perPage).take(perPage);
+
+    // return data
+    return await builder.getMany();
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productsRepository.findOne(id);
+    return product;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async update(image, id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.findOne(id);
+    const categoryId = updateProductDto.category_id;
+    const category = await this.categoriesService.findOne(categoryId);
+
+    if (!category) {
+      throw new NotFoundException();
+    }
+
+    if (!image?.filename) {
+      throw new BadRequestException();
+    }
+
+    product.name = updateProductDto.name;
+    product.model = updateProductDto.model;
+    product.original_price = updateProductDto.original_price;
+    product.price = updateProductDto.price;
+    product.image = image?.filename;
+    product.tutorial = updateProductDto.tutorial;
+    product.description = updateProductDto.description;
+    product.platform = updateProductDto.platform;
+    product.status = updateProductDto.status;
+    product.sale_of = updateProductDto.sale_of;
+    product.variant_id = updateProductDto.variant_id;
+    product.variant_title = updateProductDto.variant_title;
+    product.variant_text = updateProductDto.variant_text;
+    product.additional_information = updateProductDto.additional_information;
+    product.amount = updateProductDto.amount;
+    product.category = category;
+
+    const productSaved = await this.productsRepository.save(product);
+
+    return productSaved;
+  }
+
+  async remove(id: string, remove: boolean) {
+    const product = await this.findOne(id);
+    if (!product) {
+      throw new NotFoundException();
+    }
+
+    if (remove) {
+      await this.productsRepository.delete(product);
+    } else {
+      await this.productsRepository.softDelete(product);
+    }
   }
 }
