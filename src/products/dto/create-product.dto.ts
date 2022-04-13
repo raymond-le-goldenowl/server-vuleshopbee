cc191744c431e9a2bac37ff8656c4672a2e0ec5e
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2022-04-13 11:55:55 +0700

    feat: create, read, update, delete, get image of product entity

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,25 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+  name: string;
+  model: string;
+  original_price: number;
+  price: number;
+  slug: string;
+  image: string;
+
+  tutorial!: string;
+  description!: string;
+  platform!: string;
+
+  status: boolean;
+  sale_of: number;
+
+  variant_id!: string;
+  variant_title!: string;
+  variant_text!: string;
+
+  additional_information: string;
+
+  amount: number;
+
+  category_id: string;
+}
